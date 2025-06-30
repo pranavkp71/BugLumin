5022331439ea46c416d5d2f6d61bd71818b74ee3
pranav <pranavkp170@gmail.com>
2025-06-30 22:58:28 +0530

    Fetch and display list of snapshots

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .models import db
 from .routes import main
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buglumin.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- frontend/buglumin-frontend/src/pages/Home.js
+++ frontend/buglumin-frontend/src/pages/Home.js
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return <div className="p-4">All Snapshots</div>
+    const [snapshots, setSnapshots] = useState([]);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5000/snapshots/")
+        .then(res => res.json())
+        .then(data => {
+            console.log("Fetched snapshots:", data);
+            setSnapshots(data.snapshots);
+        })
+        .catch(err => console.error("Error fetching snapshots:", err));
+
+    }, []);
+
+    return (
+        <div className="p-4">
+            <h1 className="text-xl font-bold mb-4">Debug Snapshots</h1>
+            {snapshots.map(snap => (
+                <Link to={`/snapshot/${snap.id}`} key={snap.id}>
+                    <div className="border p-2 mb-2 rounded bg-gray-100 hover:bg-gray-200">
+                        <p className="text-sm text-gray-700">{snap.created_at}</p>
+                        <pre className="text-xs">{snap.code.slice(0,100)}...</pre>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    );
 }

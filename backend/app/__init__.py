e24d0702a8e89308f1a4a7dc4691dc4cecfa7899
pranav <pranavkp170@gmail.com>
2025-07-03 15:13:31 +0530

    Add JWT config and setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from .models import db
 from .routes import main
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -9,8 +10,11 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buglumin.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'your_super_secret_key'
 
     db.init_app(app)
+    jwt = JWTManager(app)
+    
     app.register_blueprint(main)
 
     with app.app_context():
